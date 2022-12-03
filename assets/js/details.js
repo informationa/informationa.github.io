42b71dc79b7730fe6672711c7de613084bdc3f5f
informationa <winnie.ip@emali.io>
2022-12-03 22:30:25 +0800

    add assets

---
+++ assets/js/details.js
@@ -0,0 +1,11 @@
+
+// Check status on onload
+window.onload=function(){
+    var firstVisit = localStorage.getItem("firstVisit");
+    if(firstVisit != 'false'){
+        window.location.replace("./index.html");
+        return;
+    }
+    // Display the page
+    document.getElementById('main').style.display = 'inline';
+}
